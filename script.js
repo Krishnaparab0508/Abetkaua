e746624e5df395ca57436279ed9337a7aae84f34
Krishnaparab0508 <100294584+Krishnaparab0508@users.noreply.github.com>
2024-01-05 22:52:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+var cardOne = document.querySelector(".card1");
+cardOne.addEventListener("mousemove", function () {
+    anime({
+        targets: document.querySelector(".card1"),
+        rotateY:"+=180",
+        easing:'easeInOutSine',
+        duration:400,
+    });
+})
+cardOne.addEventListener("mouseleave", function () {
+    anime({
+        targets: document.querySelector(".card1"),
+        rotateY:"+=180",
+        easing:'easeInOutSine',
+        duration:400,
+    });
+})
+var cardTwo = document.querySelector(".card2");
+cardTwo.addEventListener("mousemove", function () {
+    anime({
+        targets: document.querySelector(".card2"),
+        rotateY:"+=180",
+        easing:'easeInOutSine',
+        duration:400,
+    });
+})
+cardTwo.addEventListener("mouseleave", function () {
+    anime({
+        targets: document.querySelector(".card2"),
+        rotateY:"+=180",
+        easing:'easeInOutSine',
+        duration:400,
+    });
+})
+var cardThree = document.querySelector(".card3");
+cardThree.addEventListener("mousemove", function () {
+    anime({
+        targets: document.querySelector(".card3"),
+        rotateY:"+=180",
+        easing:'easeInOutSine',
+        duration:400,
+    });
+})
+cardThree.addEventListener("mouseleave", function () {
+    anime({
+        targets: document.querySelector(".card3"),
+        rotateY:"+=180",
+        easing:'easeInOutSine',
+        duration:400,
+    });
+})
